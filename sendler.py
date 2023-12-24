579eed2e18ee910d47a34e345a66c62874806772
Artorias-git <102858219+Artorias-git@users.noreply.github.com>
2023-12-25 02:20:22 +0800

    Add files via upload

---
+++ sendler.py
@@ -0,0 +1,52 @@
+import random
+import time
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'broker.emqx.io'
+
+topic = "house/RED_ALERT"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = 'emqx'
+password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %dn", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    msg = "d"
+    while True:
+        time.sleep(5)
+        if (msg == "d"): msg = "u"
+        else: msg = "d"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    run()
